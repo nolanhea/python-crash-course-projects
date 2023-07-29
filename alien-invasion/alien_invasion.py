bb5cabeb70ef04eb2ea3cae97a11763a0ca8a4fd
ramir512 <ramir.khvan@gmail.com>
2023-07-29 18:48:47 +0300

    Add bullets firing

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 import time
 from settings import Settings
 from ship import Ship
-
+from bullet import Bullet
 class AlienInvasion:
     """Class to manage game assets and behaviour"""
     def __init__(self):
@@ -14,6 +14,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop fo the game"""
@@ -21,7 +22,9 @@ class AlienInvasion:
             time.sleep(0.005)
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
+
 
     def _check_events(self):
         # Watch the keyboard and mouse events.
@@ -40,6 +43,13 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _check_keyup_event(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
@@ -48,7 +58,8 @@ class AlienInvasion:
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
-
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         # Make most recently drawn screen visible:
         pygame.display.flip()
